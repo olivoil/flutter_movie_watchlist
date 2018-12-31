e6691933c53cfd4e3935bf997e95a0caa62820b4
Olivier Melcher <olivier.melcher@gmail.com>
2018-12-31 03:25:28 +0000

    add static card

---
+++ lib/layout_overlays.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/widgets.dart';
+
+/// Displays an overlay Widget anchored directly above the center of this
+/// [AnchoredOverlay].
+///
+/// The overlay Widget is created by invoking the provided [overlayBuilder].
+///
+/// The [anchor] position is provided to the [overlayBuilder], but the builder
+/// does not have to respect it. In other words, the [overlayBuilder] can
+/// interpret the meaning of "anchor" however it wants - the overlay will not
+/// be forced to be centered about the [anchor].
+///
+/// The overlay built by this [AnchoredOverlay] can be conditionally shown
+/// and hidden by settings the [showOverlay] property to true or false.
+///
+/// The [overlayBuilder] is invoked every time this Widget is rebuilt.
+class AnchoredOverlay extends StatelessWidget {
+  AnchoredOverlay({
+    Key key,
+    this.showOverlay = false,
+    this.overlayBuilder,
+    this.child,
+  }) : super(key: key);
+
+  final bool showOverlay;
+  final Widget Function(BuildContext, Rect anchorBounds, Offset anchor)
+      overlayBuilder;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // This LayoutBuilder gives us the opportunity to measure the above
+      // Container to calculate the "anchor" point at its center.
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return OverlayBuilder(
+            showOverlay: showOverlay,
+            overlayBuilder: (BuildContext overlayContext) {
+              // To calculate the "anchor" point we grab the render box of
+              // our parent Container and then we find the center of that box.
+              final RenderBox box = context.findRenderObject() as RenderBox;
+              final Offset topLeft =
+                  box.size.topLeft(box.localToGlobal(const Offset(0.0, 0.0)));
+              final Offset bottomRight = box.size
+                  .bottomRight(box.localToGlobal(const Offset(0.0, 0.0)));
+              final Rect anchorBounds = Rect.fromLTRB(
+                topLeft.dx,
+                topLeft.dy,
+                bottomRight.dx,
+                bottomRight.dy,
+              );
+              final Offset anchorCenter = box.size.center(topLeft);
+
+              return overlayBuilder(overlayContext, anchorBounds, anchorCenter);
+            },
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+}
+
+/// Displays an overlay Widget as constructed by the given [overlayBuilder].
+///
+/// The overlay built by the [overlayBuilder] can be conditionally shown
+/// and hidden by settings the [showOverlay] property to true or false.
+///
+/// The [overlayBuilder] is invoked every time this Widget is rebuilt.
+///
+/// Implementation note: the reason we rebuild the overlay every time our
+/// state changes is because there doesn't seem to be any better way to
+/// invalidate the overlay itself than to invalidate this Widget. Remember,
+/// overlay Widgets exist in [OverlayEntry]s which are inaccessible to
+/// outside Widgets. But if a better approach is found then feel free to use it.
+class OverlayBuilder extends StatefulWidget {
+  OverlayBuilder({
+    Key key,
+    this.showOverlay = false,
+    this.overlayBuilder,
+    this.child,
+  }) : super(key: key);
+
+  final bool showOverlay;
+  final Widget Function(BuildContext) overlayBuilder;
+  final Widget child;
+
+  @override
+  _OverlayBuilderState createState() => _OverlayBuilderState();
+}
+
+class _OverlayBuilderState extends State<OverlayBuilder> {
+  OverlayEntry overlayEntry;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.showOverlay) {
+      // showOverlay();
+      WidgetsBinding.instance.addPostFrameCallback((_) => showOverlay());
+    }
+  }
+
+  @override
+  void didUpdateWidget(OverlayBuilder oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // syncWidgetAndOverlay();
+    WidgetsBinding.instance.addPostFrameCallback((_) => syncWidgetAndOverlay());
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    // syncWidgetAndOverlay();
+    WidgetsBinding.instance.addPostFrameCallback((_) => syncWidgetAndOverlay());
+  }
+
+  @override
+  void dispose() {
+    if (isShowingOverlay()) {
+      hideOverlay();
+    }
+
+    super.dispose();
+  }
+
+  bool isShowingOverlay() => overlayEntry != null;
+
+  void showOverlay() {
+    if (overlayEntry == null) {
+      // Create the overlay.
+      overlayEntry = OverlayEntry(
+        builder: widget.overlayBuilder,
+      );
+      addToOverlay(overlayEntry);
+    } else {
+      // Rebuild overlay.
+      buildOverlay();
+    }
+  }
+
+  Future<void> addToOverlay(OverlayEntry entry) async {
+    Overlay.of(context).insert(entry);
+  }
+
+  void hideOverlay() {
+    if (overlayEntry != null) {
+      overlayEntry.remove();
+      overlayEntry = null;
+    }
+  }
+
+  void syncWidgetAndOverlay() {
+    if (isShowingOverlay() && !widget.showOverlay) {
+      hideOverlay();
+    } else if (!isShowingOverlay() && widget.showOverlay) {
+      showOverlay();
+    }
+  }
+
+  Future<void> buildOverlay() async {
+    //overlayEntry?.markNeedsBuild();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => overlayEntry?.markNeedsBuild());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    buildOverlay();
+
+    return widget.child;
+  }
+}
+
+class CenterAbout extends StatelessWidget {
+  CenterAbout({
+    Key key,
+    this.position,
+    this.child,
+  }) : super(key: key);
+
+  final Offset position;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: position.dx,
+      top: position.dy,
+      child: FractionalTranslation(
+        translation: const Offset(-0.5, -0.5),
+        child: child,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rmdb_app/layout_overlays.dart';
 
 void main() => runApp(MyApp());
 
@@ -74,31 +75,31 @@ class _MyHomePageState extends State<MyHomePage> {
                 icon: Icons.refresh,
                 iconColor: Colors.orange,
                 onPressed: () {
-                  // TODO(olivoil):
+                  // TODO(olivoil): skip movie
                 }),
             RoundIconButton.large(
-                icon: Icons.clear,
+                icon: Icons.thumb_down,
                 iconColor: Colors.red,
                 onPressed: () {
-                  // TODO(olivoil):
+                  // TODO(olivoil): dislike
                 }),
             RoundIconButton.small(
                 icon: Icons.star,
                 iconColor: Colors.blue,
                 onPressed: () {
-                  // TODO(olivoil):
+                  // TODO(olivoil): add to list
                 }),
             RoundIconButton.large(
                 icon: Icons.favorite,
                 iconColor: Colors.green,
                 onPressed: () {
-                  // TODO(olivoil):
+                  // TODO(olivoil): like
                 }),
             RoundIconButton.small(
-                icon: Icons.lock,
+                icon: Icons.featured_video,
                 iconColor: Colors.purple,
                 onPressed: () {
-                  // TODO(olivoil):
+                  // TODO(olivoil): watch trailer
                 }),
           ],
         ),
@@ -106,11 +107,29 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget _buildCardStack() {
+    return AnchoredOverlay(
+      showOverlay: true,
+      child: Center(),
+      overlayBuilder: (BuildContext context, Rect anchorBounds, Offset anchor) {
+        return CenterAbout(
+          position: anchor,
+          child: Container(
+            width: anchorBounds.width,
+            height: anchorBounds.height,
+            padding: EdgeInsets.all(16.0),
+            child: MovieCard(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: Container(),
+      body: _buildCardStack(),
       bottomNavigationBar: _buildBottomBar(),
     );
   }
@@ -139,7 +158,7 @@ class RoundIconButton extends StatelessWidget {
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           color: Colors.white,
-          boxShadow: [
+          boxShadow: <BoxShadow>[
             BoxShadow(
               color: Color(0x11000000),
               blurRadius: 10.0,
@@ -154,6 +173,99 @@ class RoundIconButton extends StatelessWidget {
             color: iconColor,
           ),
           onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}
+
+class MovieCard extends StatefulWidget {
+  @override
+  _MovieCardState createState() => _MovieCardState();
+}
+
+class _MovieCardState extends State<MovieCard> {
+  Widget _buildBackground() {
+    return Image.asset(
+      'assets/poster.jpg',
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildMovieSynopsis() {
+    return Positioned(
+      left: 0.0,
+      right: 0.0,
+      bottom: 0.0,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+              Colors.transparent,
+              Colors.black.withOpacity(0.8),
+            ],
+          ),
+        ),
+        padding: EdgeInsets.all(24.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Fantastic Beasts: The Crimes of Grindelwald',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  Text(
+                    'Gellert Grindelwald has escaped imprisonment and has begun gathering followers to his cause—elevating wizards above all non-magical beings. The only one capable of putting a stop to him is the wizard he once called his closest friend, Albus Dumbledore. However, Dumbledore will need to seek help from the wizard who had thwarted Grindelwald once before, his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Icon(Icons.info, color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Color(0x11000000),
+              blurRadius: 5.0,
+              spreadRadius: 2.0,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Material(
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                _buildBackground(),
+                _buildMovieSynopsis(),
+              ],
+            ),
+          ),
         ),
       ),
     );
